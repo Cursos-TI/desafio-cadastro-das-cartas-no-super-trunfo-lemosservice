edaa7f0177e598e8d17b0a077f9a1c647ee70bf8
lemosservice <igorsantannalemmos@gmail.com>
2025-01-30 12:20:36 +0000

    desafio 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 int main() {
-
+     
+    Char Estado[50]
     char Codigo_Carta[10];
     char Nome_cidade[50];
     float populacao;
     float area;
+    float PIB
     
-
+    
     printf("Digite o codigo da sua carta: \n");
     scanf("%s", Codigo_Carta);
 
@@ -18,12 +20,15 @@ int main() {
     scanf("%.2f" , &populacao);
 
     printf("Digite a área(km²): \n");
-    scanf("%f", &area);
+    scanf("%.2f", &area);
+
+    printf("Digite o PIB da cidade: R$ \n");
+    scanf("%.2f", &PIB);
 
     printf("O codigo da carta è: %s\n", Codigo_Carta);
     printf("Cidade: %s\n", Nome_cidade);
     printf("População: %f(Milhões)\n", populacao);
     printf("Área(km²): %f\n", area);
-
+    printf("PIB:")
 return 0;
 }
